6d8b08c2a12c07c9b2235ea6b42ab262593549c0
dalgernon-script <bltaphorn@gmail.com>
2026-02-15 15:32:44 -0500

    Add R script to print numbers 1 to 10

---
+++ r/r_script_assignment_1.R
@@ -0,0 +1,16 @@
+# Printing one statement at a time
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+# Printing over an array with a for loop
+for (i in 1:10){
+  print(i)
+}
