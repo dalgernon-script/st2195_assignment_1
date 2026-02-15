33b81bd485eb92ef71c4500e396ee29c33d46b26
dalgernon-script <bltaphorn@gmail.com>
2026-02-15 15:45:40 -0500

    Print numbers 1-10

---
+++ python/python_script_assignment_1.py
@@ -0,0 +1,15 @@
+# Print hardcoded values
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+#Print over the range of values in the sequence, not hard-coded
+for n in range(1,11):
+    print(n)
